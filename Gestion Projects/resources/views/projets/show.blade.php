4764064b168c019c7b5df5ab355cab3b67a544ff
YassineH <hosni.yassine.yh@gmail.com>
2018-07-25 17:07:57 +0100

    resposive form

--- Gestion Projects/resources/views/projets/show.blade.php
+++ Gestion Projects/resources/views/projets/show.blade.php
@@ -3,28 +3,28 @@
 @section('content')
 
     <div class="form-group row">
-    	<label class="col-sm-2 col-form-label-lg">Intitulee :</label>
-    	<div class="col-sm-10 form-control-plaintext">{{ $projet->intitulee }}</div>
+    	<label class="col-lg-2 col-sm-12 col-form-label-lg">Intitulee :</label>
+    	<div class="col-lg-10 col-sm-12 form-control-plaintext">{{ $projet->intitulee }}</div>
     </div>
     <div class="form-group row">
-    	<label class="col-sm-2 col-form-label-lg">Description :</label>
-    	<div class="col-sm-10 form-control-plaintext">{{$projet->description}}</div>
+    	<label class="col-lg-2 col-sm-12 col-form-label-lg">Description :</label>
+    	<div class="col-lg-10 col-sm-12 form-control-plaintext">{{$projet->description}}</div>
     </div>
     <div class="form-group row">
-    	<label class="col-sm-2 col-form-label-lg">Deplacement :</label>
-    	<div class="col-sm-10 form-control-plaintext">{{($projet->deplacement=='O')?'OUI':'NON'}}</div>
+    	<label class="col-lg-2 col-sm-12 col-form-label-lg">Deplacement :</label>
+    	<div class="col-lg-10 col-sm-12 form-control-plaintext">{{($projet->deplacement=='O')?'OUI':'NON'}}</div>
     </div>
     <div class="form-group row">
-    	<label class="col-sm-2 col-form-label-lg">Date Debut :</label>
-    	<div class="col-sm-10 form-control-plaintext">{{date("M j Y h:m:s", strtotime($projet->date_debut))}}</div>
+    	<label class="col-lg-2 col-sm-12 col-form-label-lg">Date Debut :</label>
+    	<div class="col-lg-10 col-sm-12 form-control-plaintext">{{date("M j Y h:m:s", strtotime($projet->date_debut))}}</div>
     </div>
     <div class="form-group row">
-    	<label class="col-sm-2 col-form-label-lg">Date Fin :</label>
-    	<div class="col-sm-10 form-control-plaintext">{{date("M j Y h:m:s", strtotime($projet->date_fin))}}</div>
+    	<label class="col-lg-2 col-sm-12 col-form-label-lg">Date Fin :</label>
+    	<div class="col-lg-10 form-control-plaintext">{{date("M j Y h:m:s", strtotime($projet->date_fin))}}</div>
     </div>
     <div class="form-group row">
-    	<label class="col-sm-2 col-form-label-lg">Date Limite :</label>
-    	<div class="col-sm-10 form-control-plaintext">{{date("M j Y h:m:s", strtotime($projet->date_limite))}}</div>
+    	<label class="col-lg-2 col-sm-12 col-form-label-lg">Date Limite :</label>
+    	<div class="col-lg-10 col-sm-12 form-control-plaintext">{{date("M j Y h:m:s", strtotime($projet->date_limite))}}</div>
     </div>
 
     <br>
