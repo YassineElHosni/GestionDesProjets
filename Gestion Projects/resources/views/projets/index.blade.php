d0e4516f6f3d7e2b4c1f67ba99c722493c553beb
YassineH <hosni.yassine.yh@gmail.com>
2018-07-25 17:41:23 +0100

    index view responsive

--- Gestion Projects/resources/views/projets/index.blade.php
+++ Gestion Projects/resources/views/projets/index.blade.php
@@ -1,24 +1,22 @@
 @extends('layouts.structure')
 
 @section('content')
-		 <table class="table table-condensed">
+		 <table class="table table-responsive-lg">
 		 	<thead>
 		 		<tr>
-		 			<th>#</th>
-		 			<th>title</th>
+		 			<th scope="col">title</th>
 		 			{{-- <th>description</th> --}}
-		 			<th>date limite</th>
-		 			<th>date debut</th>
-		 			<th>date fin</th>
-		 			<th>deplacement</th>
-		 			<th>etat</th>
+		 			<th scope="col">date limite</th>
+		 			<th scope="col">date debut</th>
+		 			<th scope="col">date fin</th>
+		 			<th scope="col">deplacement</th>
+		 			<th scope="col">etat</th>
 		 			{{-- <th>commentaire</th> --}}
 		 		</tr>
 		 	</thead>
 		 	<tbody>
 		 		@foreach($projets as $p)
 		 		<tr>
-		 			<th scope="row">{{$p->id}}</th>
 		 			<td>{{$p->intitulee}}</td>
 		 			{{-- <td>{{$p->description}}</td> --}}
 		 			<td>{{date('d-m-Y', strtotime($p->date_limite))}}</td>
